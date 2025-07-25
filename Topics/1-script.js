8324e38d9f90371c17745d814561929dcd6cc042
Senpai <clevercoder0307@gmail.com>
2025-07-25 15:08:01 +0530

    created first two lessons

---
+++ Topics/1-script.js
@@ -0,0 +1,8 @@
+console.log("Hello World") //Log
+console.warn("Warning") //Warning
+console.error("Error") //Error
+
+
+
+const userName = "Vishal"
+console.log(`My name is ${userName}`) //Concatenation
---
+++ Topics/2-userInput.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")()
+
+const data = prompt("Enter Something:")
+console.log(data)
